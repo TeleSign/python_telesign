7c67bb6c1d83c54802bb6819a078109078f9a1c6
Luka Zitnik <lzitnik@telesign.com>
2017-02-08 22:36:32 +0100

    TSMD-7238 test json parsing

---
+++ test/rest/ResponseTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace telesign\sdk\rest;
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Psr7\Response as Psr7Response;
+use telesign\sdk\rest\Response;
+
+class ResponseTest extends TestCase {
+
+  function testInvalidJsonResolvesToNull () {
+    $response = new Response(new Psr7Response(200, [], '{'));
+    $this->assertEquals('{', $response->body);
+    $this->assertNull($response->json);
+  }
+
+  function testValidJsonResolvesToStdClass () {
+    $response = new Response(new Psr7Response(200, [], '{}'));
+    $this->assertEquals('{}', $response->body);
+    $this->assertInstanceOf(\stdClass::class, $response->json);
+  }
+
+}
