aeb06d84242db761fc5c34ec6dbf6b21a9f6bbbe
Dominic Reinhard <DReinhard@telesign.com>
2017-06-15 14:44:36 -0700

    Rename Auto Verify to App Verify.

--- examples/appverify/1_get_status_by_external_id.py
+++ examples/appverify/1_get_status_by_external_id.py
@@ -1,16 +1,16 @@
 from __future__ import print_function
-from telesign.autoverify import AutoVerifyClient
+from telesign.appverify import AppVerifyClient
 
 customer_id = "FFFFFFFF-EEEE-DDDD-1234-AB1234567890"
 api_key = "EXAMPLE----TE8sTgg45yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw=="
 
 external_id = "external_id"
 
-autoverify = AutoVerifyClient(customer_id, api_key)
-response = autoverify.status(external_id)
+appverify = AppVerifyClient(customer_id, api_key)
+response = appverify.status(external_id)
 
 if response.ok:
-    print("AutoVerify transaction with external_id {} has status code {} and status description {}.".format(
+    print("App Verify transaction with external_id {} has status code {} and status description {}.".format(
         external_id,
         response.json['status']['code'],
         response.json['status']['description']))
