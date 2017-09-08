43d981243f90de85ab63ae1db873e1b4cd40b6da
George Marshall <gmarshall@telesign.com>
2017-09-08 14:45:06 -0700

    Cleanup package setup.py and wheel support
    * Removed unneeded dependancies, replacing with compatibility shims
    * Added tox.ini file for multi-version testing
    * Cleaned up .travis.yml
    * Fixed MANIFEST.in inclusion rules

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
+import sys
+from codecs import open
+from os import path
+
 from setuptools import setup, find_packages
+
+EXCLUDE_FROM_PACKAGES = ['tests']
+
+needs_mock = sys.version_info < (3, 3)
+mock = ['mock'] if needs_mock else []
+
+here = path.abspath(path.dirname(__file__))
 
 version = "2.2.1"
 
-try:
-    with open("README") as f:
-        readme_content = f.read()
-except (IOError, Exception):
-    readme_content = ""
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='telesign',
       version=version,
-      description="Telesign SDK",
-      license="MIT License",
+      description="TeleSign SDK",
+      license="MIT",
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Intended Audience :: Developers",
           "License :: OSI Approved :: MIT License",
           "Natural Language :: English",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
@@ -26,12 +35,13 @@ setup(name='telesign',
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
       ],
-      long_description=readme_content,
+      long_description=long_description,
       keywords='telesign, sms, voice, mobile, authentication, identity, messaging',
       author='TeleSign Corp.',
       author_email='support@telesign.com',
       url="https://github.com/telesign/python_telesign",
       install_requires=['requests'],
-      tests_require=['nose', 'mock', 'pytz', 'coverage', 'codecov'],
-      packages=find_packages(exclude=['test', 'test.*', 'examples', 'examples.*']),
+      test_suite='nose.collector',
+      tests_require=['nose', 'pytz'] + mock,
+      packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
       )
