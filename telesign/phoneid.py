455d2c57e24e2cab7940d3c5c9195c2c08b1aae6
Juan Camilo Martinez <jmartinez@telesign.com>
2024-11-05 13:04:37 -0500

    Fix issue method phoneid

--- telesign/phoneid.py
+++ telesign/phoneid.py
@@ -24,31 +24,5 @@ class PhoneIdClient(RestClient):
         See https://developer.telesign.com/docs/phoneid-api for detailed API documentation.
         """
         return self.post(PHONEID_RESOURCE.format(phone_number=phone_number),
-                         **params)
-
-    def _execute(self, method_function, method_name, resource, **params):
-        resource_uri = "{api_host}{resource}".format(api_host=self.api_host, resource=resource)
-
-        json_fields = json.dumps(params)
-
-        content_type = "application/json" if method_name in ("POST", "PUT") else ""
-
-        headers = self.generate_telesign_headers(self.customer_id,
-                                                 self.api_key,
-                                                 method_name,
-                                                 resource,
-                                                 json_fields,
-                                                 user_agent=self.user_agent,
-                                                 content_type=content_type)
-
-        if method_name in ['POST', 'PUT']:
-            payload = {'data': json_fields}
-        else:
-            payload = {'params': json_fields}
-
-        response = self.Response(method_function(resource_uri,
-                                                 headers=headers,
-                                                 timeout=self.timeout,
-                                                 **payload))
-
-        return response
+                        json_fields=params,
+                        **params)
