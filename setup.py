05404b00163f43a1e58b8fb9605ee74ad5c1df64
Juan Camilo Martinez <jmartinez@telesign.com>
2024-11-06 11:21:55 -0500

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ mock = ['mock'] if needs_mock else []
 
 here = path.abspath(path.dirname(__file__))
 
-version = "2.2.3"
+version = "2.2.4"
 
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
